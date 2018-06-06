e4c2504cc34c6480a1e32e4a8e198228f4c0cc53
Sinkjaer <sinkjaermikkel@gmail.com>
2018-06-06 11:55:32 +0200

    lort

--- Exercise 4.R
+++ Exercise 4.R
@@ -9,68 +9,68 @@
 ### Det st??r vi skal lave et program der tager offered traffic??? og antal service unit som input
 ### Output for denne funktion skal formegentligt v??re rejection rate
 
-traffic =1/8
-ServiceUnits = 10
+# traffic =1/8
+# ServiceUnits = 10
 
 
-# CustomerSimulation = function(traffic,ServiceUnits){
-# Generate U
-
-N.iter = 320000
-X = vector('numeric', N.iter)
-U = vector('numeric', N.iter)
-M = 10^8
-a = 57
-c = 1
-X[1]=3
-for ( i in 1:N.iter){
-  X[i+1]=(a*X[i]+c)%%M
-  U[i+1]=X[i]/M
-}
-U = U[U!=0]
-
-lambda = traffic
-X_service = -log(U)/lambda
-X_service = X_service[is.finite(X_service)]
-X_service = X_service[!is.na(X_service)]
-
-
-## Exponential
-lambda = 1
-X_customer = -log(U)/lambda
-# ## Erlang
-# k= 8
-# D = length(U)/k
-# a = 1
-# X_customer = vector()
-# for (i in 1:D){
-#   b = a+k-1
-#   X_customer[i] = -1/lambda*log(prod(U[a:b]))
-#   a = i*k+1
-# }
-
-# ## Hyper exponential
-# X_customer = X_customer[is.finite(X_customer)]
-# X_customer = X_customer[!is.na(X_customer)]
-
-
-
-###
-
-
-indkoersel = 6000
-
-N = 10000
-a = 0
-RejectionRate = vector()
-for (i in 1:10){
-  B = 0
-  Customers = 0
-  S = vector('numeric',ServiceUnits)
-  clock = 1
-  Next_customer = 1
-  while (Customers < N){
-    a = a+1
+CustomerSimulation = function(traffic,ServiceUnits){
+  # Generate U
+  
+  N.iter = 320000
+  X = vector('numeric', N.iter)
+  U = vector('numeric', N.iter)
+  M = 10^8
+  a = 57
+  c = 1
+  X[1]=3
+  for ( i in 1:N.iter){
+    X[i+1]=(a*X[i]+c)%%M
+    U[i+1]=X[i]/M
+  }
+  U = U[U!=0]
+  
+  lambda = traffic
+  X_service = -log(U)/lambda
+  X_service = X_service[is.finite(X_service)]
+  X_service = X_service[!is.na(X_service)]
+  
+  
+  ## Exponential
+  lambda = 1
+  X_customer = -log(U)/lambda
+  # ## Erlang
+  # k= 8
+  # D = length(U)/k
+  # a = 1
+  # X_customer = vector()
+  # for (i in 1:D){
+  #   b = a+k-1
+  #   X_customer[i] = -1/lambda*log(prod(U[a:b]))
+  #   a = i*k+1
+  # }
+  
+  # ## Hyper exponential
+  # X_customer = X_customer[is.finite(X_customer)]
+  # X_customer = X_customer[!is.na(X_customer)]
+  
+  
+  
+  ###
+  
+  
+  indkoersel = 6000
+  
+  N = 10000
+  a = 0
+  RejectionRate = vector()
+  for (i in 1:10){
+    B = 0
+    Customers = 0
+    S = vector('numeric',ServiceUnits)
+    clock = 1
+    Next_customer = 1
+    while (Customers < N){
+      a = a+1
       
       while (Customers < N+indkoersel){
         a=a+1
@@ -104,10 +104,16 @@ for (i in 1:10){
         }
         
       }
-      RejectionRate[i] = (B-Start_B)/(Customers-Start_customer)
+      
     }
     
-    return(print(mean(RejectionRate)))
+    RejectionRate[i] = (B-Start_B)/(Customers-Start_customer)
+    avg = mean(RejectionRate)
+    conf_under =avg - qt(0.975, df = 9) * sd(RejectionRate) / sqrt(Customers-Start_customer)
+    
+    conf_above = avg + qt(0.975, df = 9) * sd(RejectionRate) / sqrt(Customers-Start_customer)
+    output = c(avg,conf_under,conf_above )
+    return(print(output))
   }
   
-  
+}
