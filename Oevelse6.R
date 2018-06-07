f4a11afbf79a3277cf3d46f9638f8b3b6ec59377
Sinkjaer <sinkjaermikkel@gmail.com>
2018-06-07 22:26:58 +0200

    GIBBBBBSSSS!!!

--- Oevelse6.R
+++ Oevelse6.R
@@ -22,6 +22,7 @@ for (i in 2:N.iter){
   }
 }
 hist(X)
+plot(g)
 
 densityX = rle(sort(X))
 densityX = densityX[["lengths"]]/N.iter
@@ -72,5 +73,103 @@ chisq.test(densityX,c(g)) # p-value = 0.2504
 plot(g)
 
 
+######### GIBBS
+A1 = 4
+A2 = 4
+ii = 1:5
+jj = 1:5
+g = matrix(NA, 10, 10)
+for (j in 1:10){
+  for ( i in 1:10){
+    g[i,j] = (A1^i)/factorial(i)*(A2^i)/factorial(j)
+  }
+}
+x =c(1,1)
+X = matrix(x,1, 2)
+N.iter = 10000
+n = 10
+for (i in 2:N.iter){
+  Y = floor(runif(2,0,11))
+  Y
+  if (g[Y+1][1]>=g[x+1][1]){
+    x[1]=Y[1]
+  } else {
+    E = runif(2,0,1)
+    if (E[1] < g[Y][1]/g[x][1]){
+      x[1] = Y[1]
+    }
+  }
+  if (g[Y+1][2]>=g[x+1][2]){
+    x[2]=Y[2]
+  } else {
+    E = runif(2,0,1)
+    if (E[2] < g[Y][2]/g[x][2]){
+      x[2] = Y[2]
+    }
+  }
+  X = rbind(X,x)
+}
+
+densityX = vector()
+
+for (i in 1:10){
+  for (j in 1:10){
+    densityX = c(densityX,length(X[X==c(i,j)]))
+  }
+}
+densityX=densityX/N.iter
+
+# dev.new()
+hist(X, freq = FALSE)
+chisq.test(densityX,c(g)) # p-value = 0.2504
+plot(g)
 
 
+######### abr dim func
+abrmarkov = function(dimension,callType,X0,n){
+  
+  # dimension = 4
+  # callType = c(4,4,4,4)
+  # dimension = 4
+  # X0 =c(3,3,3,3)
+  # n =10
+  # 
+  gFunction = function(cord,callType,dimension){
+    
+    g = 1
+    for (i in 1:dimension){
+    g = g*(callType[i]^(cord[i]))/factorial(cord[i])
+    
+    }
+    return(g)
+  }
+  
+  x = X0
+  X = matrix(X,1, length(x))
+  N.iter = 10000
+  for (i in 2:N.iter){
+    Y = floor(runif(length(x),0,n+1))
+    Y
+    if (all( gFunction(Y,callType,dimension)>= gFunction(x,callType,dimension))){
+      x=Y
+    } else {
+      E = runif(length(x),0,1)
+      if (all(E < gFunction(Y,callType,dimension)/gFunction(x,callType,dimension))){
+        x = Y
+      }
+    }
+    X = rbind(X,x)
+  }
+  
+  densityX = vector()
+  
+  
+  result = X
+  return(result)
+  
+}
+
+
+plot(X[,1],X[,3])
+
+X
