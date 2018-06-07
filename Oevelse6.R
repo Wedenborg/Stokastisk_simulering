6a1e6b1db6ce604de334c1998fe2881ffc554f5b
Sinkjaer <sinkjaermikkel@gmail.com>
2018-06-07 19:45:46 +0200

    chi

--- Oevelse6.R
+++ Oevelse6.R
@@ -1,6 +1,6 @@
-#### Øvelse 6
+#### ??velse 6
 
-## Det første!!
+## Det f??rste!!
 A = rep(8,10)
 AA = 8
 for (j in 0:length(A)){
@@ -23,39 +23,54 @@ for (i in 2:N.iter){
 }
 hist(X)
 
+densityX = rle(sort(X))
+densityX = densityX[["lengths"]]/N.iter
+chisq.test(densityX,g) # p-value = 0.2423
 
-# ######### DET NYE
-# A1 = 4
-# A2 = 4
-# ii = 1:5
-# jj = 1:5
-# g = matrix(NA, 10, 10)
-# for (j in 1:10){
-#   for ( i in 1:10){
-#     g[i,j] = (A1^i)/factorial(i)*(A2^i)/factorial(j)
-#   }
-# }
-# x =c(1,1)
-# X = matrix(x,1, 2)
-# N.iter = 10000
-# n = 10
-# for (i in 2:N.iter){
-#   Y = floor(runif(2,0,11))
-#   Y
-#   if (all(g[Y+1]>=g[x+1])){
-#     x=Y
-#   } else {
-#     E = runif(2,0,1)
-#     if (all(E < g[Y]/g[x])){
-#       x = Y
-#     }
-#   }
-#   X = rbind(X,x)
-# }
-#
-#
-#
+
+######### i og j
+A1 = 4
+A2 = 4
+ii = 1:5
+jj = 1:5
+g = matrix(NA, 10, 10)
+for (j in 1:10){
+  for ( i in 1:10){
+    g[i,j] = (A1^i)/factorial(i)*(A2^i)/factorial(j)
+  }
+}
+x =c(1,1)
+X = matrix(x,1, 2)
+N.iter = 10000
+n = 10
+for (i in 2:N.iter){
+  Y = floor(runif(2,0,11))
+  Y
+  if (all(g[Y+1]>=g[x+1])){
+    x=Y
+  } else {
+    E = runif(2,0,1)
+    if (all(E < g[Y]/g[x])){
+      x = Y
+    }
+  }
+  X = rbind(X,x)
+}
+
+densityX = vector()
+
+for (i in 1:10){
+  for (j in 1:10){
+    densityX = c(densityX,length(X[X==c(i,j)]))
+  }
+}
+densityX=densityX/N.iter
+
 # dev.new()
-# hist(X, freq = FALSE)
-#
-# plot(g)
+hist(X, freq = FALSE)
+chisq.test(densityX,c(g)) # p-value = 0.2504
+plot(g)
+
+
+
+
